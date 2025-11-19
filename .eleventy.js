fb864fdbc4221ed8708773b2ce564d23538105f9
KoningGeert <geertverstijnen@icloud.com>
2025-11-19 11:03:02 +0100

    New landingspage

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 module.exports = function(eleventyConfig) {
+  // 1. PASSTHROUGH COPY
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/scripts");
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/pages");
-
+  // 3. RETURN OBJECT (AFSLUITING VAN DE FUNCTIE)
   return {
     dir: {
       input: "src",
